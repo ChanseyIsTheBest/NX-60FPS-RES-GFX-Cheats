493d25686bb5d2adba377a846a0f2588690775bb
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-12-05 21:51:45 +1000

    Order games alphabetically

--- generateGamesList.py
+++ generateGamesList.py
@@ -15,8 +15,8 @@ Currently contains {numCheats} cheats for {numTitles} titles.
 | --- | --- | --- | --- | --- |
 """
 
-i = 1
 numCheats = 0
+tableItems = []
 
 for title in os.listdir("titles"):
     cheats = [file.removesuffix(".txt") for file in os.listdir(os.path.join("titles", title, "cheats"))]
@@ -35,12 +35,14 @@ for title in os.listdir("titles"):
     versionsLinked = [f"[{version[0]}](titles/{title}/cheats/{version[1]}.txt)" for version in versions]
 
     nameLink = urllib.parse.quote(f"titles/{title}/{name}.txt")
-    table += f"| {i} | [{name}]({nameLink}) | [{title}](titles/{title}) | {', '.join(cheatsLinked)} | {', '.join(versionsLinked)} | \n"
-    i += 1
+    tableItems.append(f"[{name}]({nameLink}) | [{title}](titles/{title}) | {', '.join(cheatsLinked)} | {', '.join(versionsLinked)} |")
     numCheats += len(cheats)
 
 table = table.replace("{numCheats}", str(numCheats))
-table = table.replace("{numTitles}", str(i - 1))
+table = table.replace("{numTitles}", str(len(tableItems)))
+
+tableItems.sort()
+table += "\n".join([f"| {i+1} | {item}" for i, item in enumerate(tableItems)])
 
 if(os.path.exists("GAMES.md")):
     existingGames = open("GAMES.md", "r", encoding="utf-16").read()
